99ce156a4a6a74ea2bdd652db6335dcc92108d9a
nhthu <nhthung1012@gmail.com>
2024-04-18 12:39:39 +0700

    chang type

--- process.py
+++ process.py
@@ -10,13 +10,13 @@ import pandas as pd
 
 csv_file_path = './output/roomingHouse.csv'
 df = pd.read_csv(csv_file_path)
- 
+# df.drop('year', inplace=True, axis=1)
 # Step 2: Define the values for the new "City" column
 newVal = []
 for i in range(1,12001):
-    if (i <= 4001): newVal.append('can-ho')
-    elif (i <= 8001): newVal.append('nha-dat')
-    else: newVal.append('phong-tro')
+    if (i <= 4001): newVal.append('can_ho')
+    elif (i <= 8001): newVal.append('nha_dat')
+    else: newVal.append('phong_tro')
  
 # Step 3: Add the new "City" column to the DataFrame
 df['type'] = newVal
